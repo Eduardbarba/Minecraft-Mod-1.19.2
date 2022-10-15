d68367fd4927d4fda5f9daf13af232579eb59686
Eduardbarba <eduard.barba11@gmail.com>
2022-10-14 19:33:44 -0700

    Initial commit

---
+++ src/main/java/net/eduard/customblock/CustomBlockClient.java
@@ -0,0 +1,10 @@
+package net.eduard.customblock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomBlockClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
